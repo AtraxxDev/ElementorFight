2b3a65a9f5e1230cc941932a2af6019eb055db16
Elliot Reyes <gametraxworks@gmail.com>
2023-10-01 23:41:54 -0600

    Arreglando comentarios y health bar

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -7,6 +7,7 @@ public class Health : MonoBehaviour
     [SerializeField] private float _maxHealth = 3;
     [SerializeField] private GameObject _deadEffect,_hitEffect;
     private float _currentHealth;
+    public Animator _animator;
 
     [SerializeField] private HealthBar _healthBar;
 
@@ -27,14 +28,16 @@ public class Health : MonoBehaviour
     {
         if (_currentHealth <= 0)
         {
-            // se instancia el efecto de muerte
-            // anim muerte 
+            // se instancia el efecto de muerte (Particle)
+            // Active Ragdoll cuando muere 
+            // se le activa o calcula la fuerza para que salga volando
         }
 
         else
         {
-            _healthBar.UpdateHealthBar(_maxHealth, _currentHealth);
-            // se pone efecto de hit damage
+            _healthBar.UpdateHealthBar(_maxHealth, _currentHealth); // Se va actualizando la vida mediante el daño
+            // se pone efecto de hit damage (Particle)
+            _animator.SetTrigger("_Hit"); // manda la animacion _HIt
         }
     }
 }
--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -8,7 +8,7 @@ public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Image _healthbarSprite;
     [SerializeField] private float _reduceSpeed = 2 ;
-    private float _target;
+    private float _target = 1;
     // Start is called before the first frame update
     public void UpdateHealthBar(float maxHealth, float currentHealth)
     {
@@ -16,6 +16,6 @@ public class HealthBar : MonoBehaviour
     }
     private void Update()
     {
-        _healthbarSprite.fillAmount = Mathf.MoveTowards(_healthbarSprite.fillAmount, _target, _reduceSpeed * Time.deltaTime);
+        _healthbarSprite.fillAmount = Mathf.MoveTowards(_healthbarSprite.fillAmount, _target, _reduceSpeed * Time.deltaTime); // Reduce el fill de la vida como si fuera un lerp
     }
 }
