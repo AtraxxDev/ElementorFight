fb939f444188685edfd84af45a5b87eafd8a909b
ELMEROMERO <cdmx2125@amerike.edu.mx>
2023-10-03 18:45:26 -0600

    Jijija

---
+++ Assets/Scripts/Punch.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punch : MonoBehaviour
+{
+    
+    [SerializeField] private Health _health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("P"))
+        {
+
+        }
+    }
+}
